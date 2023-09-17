e17dbf68d75c693cc20c4eb384efe213ccb9e84b
DenysTkachov <D.Tkachev@ukr.net>
2023-09-17 16:44:58 +0200

    3

---
+++ src/js/03-promises.js
@@ -0,0 +1,38 @@
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+document.querySelector('.form').addEventListener('submit', e => {
+  e.preventDefault();
+
+  const delayInput = document.querySelector('[name="delay"]');
+  const stepInput = document.querySelector('[name="step"]');
+  const amountInput = document.querySelector('[name="amount"]');
+
+  const initialDelay = parseInt(delayInput.value);
+  const step = parseInt(stepInput.value);
+  const amount = parseInt(amountInput.value);
+
+  for (let i = 0; i < amount; i++) {
+    const position = i + 1;
+    const delay = initialDelay + step * i;
+
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+});
